4cfa869650d941a38afb718a1adbdb038d4d912a
ceenobi <cobimbachu@gmail.com>
2022-02-12 21:19:03 +0100

    json

--- pages/register.js
+++ pages/register.js
@@ -53,7 +53,7 @@ export default function Register() {
         password,
       })
       dispatch({ type: 'USER_LOGIN', payload: data })
-      Cookies.set('userInfo', data)
+      Cookies.set('userInfo', JSON.stringify(data))
         setLoading(false)
       router.push(redirect || '/')
     } catch (err) {
